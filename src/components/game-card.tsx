d6bdb6062375a8d3aed175223e49997556993b42
CapEugene <evgexa_2003@bk.ru>
2025-05-05 17:10:15 +0300

    feat(ui): create GameCard component with responsive design

---
+++ src/components/game-card.tsx
@@ -0,0 +1,25 @@
+import { GameData } from "@/types/game";
+import Link from "next/link";
+import Image from "next/image";
+
+export function GameCard({ id, rating, title, info }: GameData) {
+  return (
+    <Link href={`/game/${id}`}>
+      <div className="bg-[#F6A06B] rounded-md p-3 w-100 min-h-100 h-full flex flex-col hover:shadow-md transition-shadow">
+        <div className="flex justify-center my-4">
+          <div className="bg-white p-2 rounded-md w-60 h-60 flex items-center justify-center">
+            <Image
+              src="../../public/file.svg"
+              alt={title}
+              width={48}
+              height={48}
+            />
+          </div>
+        </div>
+        <h2 className="text-center text-black font-medium text-3xl mb-1">{title}</h2>
+        <span className="inline-block text-black text-center font-medium text-2xl mb-1 mx-auto px-4">{rating}/10</span>
+        <p className="text-s text-black">{info}</p>
+      </div>
+    </Link>
+  );
+}
