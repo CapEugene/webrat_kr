9d57111ec497342c9d192a0c8a51fe503bd9b9a6
CapEugene <evgexa_2003@bk.ru>
2025-05-05 17:08:28 +0300

    feat(games): add game data types and mock dataset

---
+++ src/types/game.ts
@@ -0,0 +1,10 @@
+export interface GameData {
+    id: number;
+    title: string;
+    rating: number;
+    info: string;
+    genre?: string;
+    publisher?: string;
+    developer?: string;
+    platform?: string;
+  }
